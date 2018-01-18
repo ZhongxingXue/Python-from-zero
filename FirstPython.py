81405060bfc8eef114c2982dc7d6bf9aa83c838d
Zhongxing Xue <zx2221@columbia.edu>
2018-01-18 15:18:39 -0500

    LIST and FOR

--- FirstPython.py
+++ FirstPython.py
@@ -1,4 +1,13 @@
 
-First_list = {"AAA","BBB","CCC","DDD"}
+First_list = ["12345", "12tef", "13dfs", "112rf", "12345"]
 print(First_list)
 
+print(First_list[1])
+
+First_list.insert(1, "13dd3")
+First_list.append("11r3t")
+
+print(First_list)
+
+for i in First_list:
+    print(i)
